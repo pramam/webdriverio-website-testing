8b218f7d904f6ebb076918e311e5d54589691933
pramam <57686495+pramam@users.noreply.github.com>
2022-01-22 14:08:43 -0800

    YouTube:TC_Search_1: Test if search term brings up expected video on first page

--- test/specs/YouTube/search.e2e.js
+++ test/specs/YouTube/search.e2e.js
@@ -1,15 +1,28 @@
 describe("Testing Search functionality on a specific video", ()=>{
 
-    it("TC_Search_1a: Test if search term brings up results", async ()=>{
+    it("TC_Search_1: Test if search term brings up expected video on first page", async ()=>{
         await browser.url("https://www.youtube.com");
         
         const elInput = await $('input[id="search"]')
 
         await elInput.setValue("Yuval Noah Harari");
+        
+        // This is just to see it visually in action
         await browser.pause(1000);
-        await elInput.keys("\uE007")
+        await elInput.keys("\uE007");
+        
         console.log (await elInput.getValue());
+
         //Wait to see the results show up in the browser
-        await browser.pause(5000);      
+        await browser.pause(5000);     
+
+        const elSearchedVideo = await $('a[title="Yuval Noah Harari on The Future of Humanity"]');
+        await elSearchedVideo.click();
+        
+        //Wait to see the results show up in the browser
+        await browser.pause(5000);
+
+        expect(await browser.getUrl()).toEqual("https://www.youtube.com/watch?v=umnMHQPYEmA");
+        // console.log(await browser.getUrl().getText());
     })
 })
