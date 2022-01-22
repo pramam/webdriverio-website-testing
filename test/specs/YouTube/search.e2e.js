410a4f20daed38644e3678244d8eb46b925e8466
pramam <57686495+pramam@users.noreply.github.com>
2022-01-22 14:21:21 -0800

    YouTube:TC_Search_4: Test if Search Term's video result takes you to the video on clicking the title of the video

--- test/specs/YouTube/search.e2e.js
+++ test/specs/YouTube/search.e2e.js
@@ -10,7 +10,7 @@ describe("Testing Search functionality on a specific video", ()=>{
         // This is just to see it visually in action
         await browser.pause(1000);
         await elInput.keys("\uE007");
-        
+
         console.log (await elInput.getValue());
 
         //Wait to see the results show up in the browser
@@ -24,5 +24,30 @@ describe("Testing Search functionality on a specific video", ()=>{
 
         expect(await browser.getUrl()).toEqual("https://www.youtube.com/watch?v=umnMHQPYEmA");
         // console.log(await browser.getUrl().getText());
+    })
+
+    it("TC_Search_4: Test if Search Term's video result takes you to the video on clicking the title of the video", async ()=>{
+        await browser.url("https://www.youtube.com");
+        
+        const elInput = await $('input[id="search"]')
+
+        await elInput.setValue("Yuval Noah Harari");
+        
+        // This is just to see it visually in action
+        await browser.pause(1000);
+        await elInput.keys("\uE007");
+
+        console.log (await elInput.getValue());
+
+        //Wait to see the results show up in the browser
+        await browser.pause(5000);     
+
+        const elSearchedVideo = await $('yt-formatted-string=Yuval Noah Harari on The Future of Humanity');
+        await elSearchedVideo.click();
+
+        //Wait to see the results show up in the browser
+        await browser.pause(5000);
+
+        expect(await browser.getUrl()).toEqual("https://www.youtube.com/watch?v=umnMHQPYEmA");  
     })
 })
